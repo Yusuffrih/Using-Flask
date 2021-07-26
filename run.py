ba7b3440079b34491cb0ce135adae8a68ecd216d
Yusuffrih <yusuffrih@hotmail.com>
2021-07-26 16:12:33 +0000

    Made changes

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -12,8 +13,21 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About",
-                           list_of_numbers=[1, 2, 3])
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as data_json:
+        data = json.load(data_json)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return "<h1>" + member["name"] + "</h1>"
 
 
 @app.route("/contact")
